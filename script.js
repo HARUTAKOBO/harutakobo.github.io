79b57d7c3df5d05a12fa423b3cd3cda556e4605b
HARUTAKOBO <150135986+HARUTAKOBO@users.noreply.github.com>
2023-11-07 21:47:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const y = parseFloat(yInput.value);
         if (!isNaN(x) && !isNaN(y)) {
             const result = (x * (1 + 0.00015282 * y)).toFixed(2);
-            resultElement.textContent = `税引後<br>${result}`; // 結果を改行して表示
+            resultElement.textContent = `税引後\n${result}`; // 結果を改行して表示
         } else {
             resultElement.textContent = "有効な数値を入力してください。";
         }
